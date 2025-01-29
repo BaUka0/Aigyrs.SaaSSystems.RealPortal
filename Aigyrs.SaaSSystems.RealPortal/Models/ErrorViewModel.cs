75d90d88f913c3801f87c0282ac393e4919d93e7
BaUka0 <bauirzhan.bisanov@gmail.com>
2025-01-29 23:08:09 +0500

    Добавьте файлы проекта.

---
+++ Aigyrs.SaaSSystems.RealPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aigyrs.SaaSSystems.RealPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
